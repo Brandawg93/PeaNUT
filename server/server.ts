0fe73ff2b05d2438a43f3bb348df878d805d36f7
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2023-09-08 17:20:48 -0400

    no cors on prod

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,9 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.WEB_PORT || 8080;
 
-app.use(cors());
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors());
+}
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
