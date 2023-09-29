c15b44e59e03f2fc0ce8b4508f2568989d87db01
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2023-09-28 20:18:33 -0400

    initial api for homepage

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import { createHandler } from 'graphql-http/lib/use/express';
 import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Nut } from './nut';
 import schema from './schema';
 
 dotenv.config();
@@ -23,6 +24,34 @@ app.get('/api/ping', (req: Request, res: Response) => {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('/api/v1/devices/:device', async (req, res) => {
+  const device = req.params.device;
+  const nut = new Nut(process.env.NUT_HOST || 'localhost', parseInt(process.env.NUT_PORT || '3493'));
+  await nut.connect();
+  const devices = await nut.getDevices();
+  if (!devices.includes(device)) {
+    res.status(404).send(`Device ${device} not found`);
+    return;
+  }
+  const data = await nut.getData(device);
+  await nut.close();
+  res.send(data);
+});
+
+app.get('/api/v1/devices', async (req, res) => {
+  const nut = new Nut(process.env.NUT_HOST || 'localhost', parseInt(process.env.NUT_PORT || '3493'));
+  await nut.connect();
+  const devices = await nut.getDevices();
+  const promises = [];
+  for (const device of devices) {
+    const promise = nut.getData(device);
+    promises.push(promise);
+  }
+  const data = await Promise.all(promises);
+  await nut.close();
+  res.send(data);
 });
 
 app.all('/graphql', createHandler({ schema }));
