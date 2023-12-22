3099f0bb23123a389b9f4f0be5e1821fc7ee8976
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2023-12-21 20:37:16 -0500

    jest

---
+++ __tests__/client/components/navbar.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import NavBar from '@/client/components/navbar'
+
+const devices = [
+  {
+    'device.serial': '1234',
+    'device.mfr': 'test',
+    'device.model': 'test',
+  },
+]
+
+describe('NavBar', () => {
+  it('renders', () => {
+    const lng = 'en'
+    render(<NavBar devices={devices} />)
+
+    const heading = screen.getByText('PeaNUT')
+
+    expect(heading).toBeInTheDocument()
+  })
+})
---
+++ __tests__/page.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import Page from '@/app/[[...lng]]/page'
+
+describe('Page', () => {
+  it('renders a heading', () => {
+    const lng = 'en'
+    const page = render(<Page params={lng} />)
+
+    expect(page).toBeDefined()
+  })
+})
