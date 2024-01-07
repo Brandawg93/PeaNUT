d1eda14b427253cfadd7dacb0af5341910b49e6c
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-01-06 21:24:54 -0500

    merge

--- src/client/components/grid.tsx
+++ src/client/components/grid.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Card, Typography } from '@material-tailwind/react'
+import { Card, IconButton, Typography } from '@material-tailwind/react'
+import { PencilSquareIcon } from '@heroicons/react/24/solid'
 import { useTranslation } from 'react-i18next'
-import { DEVICE } from '@/common/types'
+import { DEVICE, VARS } from '@/common/types'
 
 type TableProps = {
   key: string
@@ -21,10 +22,16 @@ export default function NutGrid(props: Props) {
     return null
   }
   let result: Array<TableProps> = []
-  result = Object.entries(data)
+  result = Object.entries(data.vars)
     .filter(([k, v]) => k !== '__typename')
     .map(([k, v]) => ({ key: k, value: v || 'N/A' }))
   result.shift()
+
+  const editBtn = (
+    <IconButton variant='filled' className='dark:border-gray-500 dark:text-gray-100'>
+      <PencilSquareIcon className='h-4 w-4 text-gray-800 dark:text-gray-100' />
+    </IconButton>
+  )
 
   return (
     <Card className='border-neutral-300 w-full overflow-scroll border border-solid border-gray-300 shadow-none dark:border-gray-800 dark:bg-gray-950'>
@@ -32,12 +39,10 @@ export default function NutGrid(props: Props) {
         <thead>
           <tr>
             {Object.keys(result[0]).map((head: string, index: number) => {
-              const isLast = index === Object.keys(result[0]).length - 1
-              const lastClass = isLast ? '' : 'border-r'
               return (
                 <th
                   key={head}
-                  className={`border-b bg-gray-400 dark:border-gray-600 dark:bg-gray-700 ${lastClass} border-neutral-300 p-3`}
+                  className={`border-neutral-300 border-b border-r bg-gray-400 p-3 dark:border-gray-600 dark:bg-gray-700`}
                 >
                   <Typography className='mb-0 text-lg font-semibold text-black dark:text-white'>
                     {t(`grid.${head}`)}
@@ -45,19 +50,26 @@ export default function NutGrid(props: Props) {
                 </th>
               )
             })}
+            <th className='border-neutral-300 w-[65px] border-b bg-gray-400 p-3 dark:border-gray-600 dark:bg-gray-700'>
+              <Typography className='mb-0 text-lg font-semibold text-black dark:text-white'>&nbsp;</Typography>
+            </th>
           </tr>
         </thead>
         <tbody>
           {result.map(({ key, value }, index: number) => {
             const isLast = index === result.length - 1
             const lastClass = isLast ? '' : 'border-b dark:border-gray-800'
+            const isRW = data.rwVars.includes(key as keyof VARS)
             return (
               <tr key={key}>
                 <td className={`p-3 ${lastClass} border-neutral-300 border-r dark:border-gray-800`}>
                   <Typography className='mb-0 font-normal dark:text-white'>{key}</Typography>
                 </td>
-                <td className={`p-3 ${lastClass} border-neutral-300`}>
+                <td className={`p-3 ${lastClass} border-neutral-300 border-r dark:border-gray-800`}>
                   <Typography className='mb-0 font-normal dark:text-white'>{value}</Typography>
+                </td>
+                <td className={`p-3 ${lastClass} border-neutral-300 w-[65px]`}>
+                  <Typography className='mb-0 font-normal dark:text-white'>{isRW ? editBtn : null} </Typography>
                 </td>
               </tr>
             )
@@ -66,6 +78,4 @@ export default function NutGrid(props: Props) {
       </table>
     </Card>
   )
-
-  // return <Grid data={result || (() => new Promise(() => {}))} columns={[{ name: 'key' }, { name: 'value' }]} sort />
 }
