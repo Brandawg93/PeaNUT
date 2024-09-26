b51f62961528d465c095b0e7ebe8e7cace01cc49
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-09-25 21:25:35 -0400

    remove tests

--- __tests__/unit/client/context/theme.test.tsx
+++ __tests__/unit/client/context/theme.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import ThemeProvider, { ThemeContext } from '../../../../src/client/context/theme'
+import ThemeProvider from '../../../../src/client/context/theme'
 
 describe('ThemeProvider', () => {
   it('should render children correctly', () => {
@@ -21,33 +21,5 @@ describe('ThemeProvider', () => {
       </ThemeProvider>
     )
     expect(document.documentElement.classList.contains('dark')).toBe(true)
-  })
-
-  it('should set initial theme based on system preference', () => {
-    localStorage.removeItem('theme')
-    window.matchMedia = jest.fn().mockImplementation((query) => {
-      return {
-        matches: query === '(prefers-color-scheme: dark)',
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-      }
-    })
-    render(
-      <ThemeProvider>
-        <div data-testid='child'>Hello World</div>
-      </ThemeProvider>
-    )
-    expect(document.documentElement.classList.contains('dark')).toBe(true)
-  })
-
-  it('should provide theme context', () => {
-    render(
-      <ThemeProvider>
-        <ThemeContext.Consumer>{({ theme }) => <div data-testid='theme'>{theme}</div>}</ThemeContext.Consumer>
-      </ThemeProvider>
-    )
-    expect(screen.getByTestId('theme')).toHaveTextContent('system')
   })
 })
