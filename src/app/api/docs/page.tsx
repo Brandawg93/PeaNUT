2d5af7678471135bebfd5afce660c82eac621091
Brandon Mcfarlin <mcfarlin@adobe.com>
2024-10-23 14:24:46 -0400

    ssr for swagger

--- src/app/api/docs/page.tsx
+++ src/app/api/docs/page.tsx
@@ -4,9 +4,5 @@ import ReactSwagger from './react-swagger'
 
 export default async function IndexPage() {
   const spec = await getApiDocs()
-  return (
-    <section className='container'>
-      <ReactSwagger spec={spec} />
-    </section>
-  )
+  return <ReactSwagger spec={spec} />
 }
--- src/app/api/docs/react-swagger.tsx
+++ src/app/api/docs/react-swagger.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React from 'react'
 import SwaggerUI from 'swagger-ui-react'
 import 'swagger-ui-react/swagger-ui.css'
