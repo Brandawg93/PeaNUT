fd5d1403837ebad8cce0b7015385842199fda03f
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-11-06 17:10:47 -0500

    next 14 downgrade

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
   eslint: {
     dirs: ['src', '__tests__'],
   },
+  experimental: {
+    instrumentationHook: true,
+  },
 }
 
 module.exports = process.env.ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig
