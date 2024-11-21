b51adfb6e882f24c00baefc2f6b2f30cdb1fd335
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-11-21 14:42:50 -0500

    more tests

---
+++ __tests__/unit/client/components/settings-wrapper.test.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import SettingsWrapper from '@/client/components/settings-wrapper'
+import { LanguageContext } from '@/client/context/language'
+// import { useRouter } from 'next/navigation'
+
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      replace: jest.fn(),
+    }
+  },
+}))
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([{ name: '1.0.0' }]),
+  })
+) as jest.Mock
+
+const mockCheckSettingsAction = jest.fn()
+const mockGetSettingsAction = jest.fn()
+const mockSetSettingsAction = jest.fn()
+
+const renderComponent = () =>
+  render(
+    <LanguageContext.Provider value='en'>
+      <SettingsWrapper
+        checkSettingsAction={mockCheckSettingsAction}
+        getSettingsAction={mockGetSettingsAction}
+        setSettingsAction={mockSetSettingsAction}
+      />
+    </LanguageContext.Provider>
+  )
+
+describe('SettingsWrapper', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('renders the settings wrapper component', async () => {
+    mockCheckSettingsAction.mockResolvedValue(true)
+    mockGetSettingsAction.mockResolvedValueOnce('localhost')
+    mockGetSettingsAction.mockResolvedValueOnce(8080)
+
+    renderComponent()
+
+    await waitFor(() => {
+      expect(screen.getByTestId('settings-wrapper')).toBeInTheDocument()
+    })
+  })
+
+  // it('redirects to login if settings check fails', async () => {
+  //   const router = useRouter()
+  //   mockCheckSettingsAction.mockResolvedValue(false)
+
+  //   renderComponent()
+
+  //   await waitFor(() => {
+  //     expect(router.replace).toHaveBeenCalledWith('/login')
+  //   })
+  // })
+
+  it('loads server settings if settings check passes', async () => {
+    mockCheckSettingsAction.mockResolvedValue(true)
+    mockGetSettingsAction.mockResolvedValueOnce('localhost')
+    mockGetSettingsAction.mockResolvedValueOnce(8080)
+
+    renderComponent()
+
+    await waitFor(() => {
+      expect(screen.getByText('Manage Servers')).toBeInTheDocument()
+      // expect(screen.getByText('localhost')).toBeInTheDocument()
+      // expect(screen.getByText('8080')).toBeInTheDocument()
+    })
+  })
+
+  it('adds a new server when add server button is clicked', async () => {
+    mockCheckSettingsAction.mockResolvedValue(true)
+    mockGetSettingsAction.mockResolvedValueOnce('localhost')
+    mockGetSettingsAction.mockResolvedValueOnce(8080)
+
+    renderComponent()
+
+    await waitFor(() => {
+      expect(screen.getByText('Manage Servers')).toBeInTheDocument()
+    })
+
+    fireEvent.click(screen.getByTitle('settings.addServer'))
+
+    // await waitFor(() => {
+    //   expect(screen.getAllByText('localhost').length).toBe(1)
+    //   expect(screen.getAllByText('8080').length).toBe(1)
+    // })
+  })
+})
--- __tests__/unit/client/components/wrapper.test.tsx
+++ __tests__/unit/client/components/wrapper.test.tsx
@@ -13,11 +13,11 @@ jest.mock('@tanstack/react-query', () => ({
   useQuery: jest.fn(),
 }))
 
-jest.mock('../../../../src/client/components/footer', () => {
-  const MockFooter = () => <div data-testid='mock-footer'>Mock Footer</div>
-  MockFooter.displayName = 'MockFooter'
-  return MockFooter
-})
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve([{ name: '1.0.0' }]),
+  })
+) as jest.Mock
 
 describe('Wrapper Component', () => {
   const mockDevicesData = {
