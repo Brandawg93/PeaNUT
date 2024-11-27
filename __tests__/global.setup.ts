3eee1c1d377489c6ea24dadbd2f4f06d9ad377f6
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-11-27 12:25:13 -0500

    remove playwright setup

--- __tests__/global.setup.ts
+++
@@ -1,6 +0,0 @@
-import { test as setup } from '@playwright/test'
-import { YamlSettings } from '@/server/settings'
-
-setup('create new database', () => {
-  new YamlSettings('./config/settings.yml')
-})
