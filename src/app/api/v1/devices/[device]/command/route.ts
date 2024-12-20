5e00235e1aa8b0c3e22cd1109b12c607d9f174b8
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2024-12-20 17:29:00 -0500

    feat(api): add POST endpoint to run commands on devices

---
+++ src/app/api/v1/devices/[device]/command/route.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { DEVICE } from '@/common/types'
+import { getSingleNutInstance } from '@/app/api/utils'
+
+type Params = {
+  device: string
+  param: keyof DEVICE
+}
+
+/**
+ * Saves value for a specific var.
+ *
+ * @swagger
+ * /api/v1/devices/{device}/command:
+ *   post:
+ *     summary: Run command on device
+ *     parameters:
+ *       - in: path
+ *         name: device
+ *         required: true
+ *         description: The ID of the device
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: The command to run
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: string
+ *     responses:
+ *       '200':
+ *         description: Successful response with command run
+ *       '500':
+ *         description: Failed to run command
+ *     tags:
+ *       - Devices
+ */
+export async function POST(request: NextRequest, { params }: { params: Promise<Params> }) {
+  const { device } = await params
+  const nut = await getSingleNutInstance(device)
+  const value = await request.json()
+
+  try {
+    const deviceExists = await nut?.deviceExists(device)
+    if (!deviceExists) {
+      return NextResponse.json(`Device ${device} not found on any instance`, { status: 404 })
+    }
+
+    // Only save the variable on the first instance that has the device
+    await nut?.runCommand(device, value)
+    return NextResponse.json(`Command ${value} on device ${device} run successfully on device ${device}`)
+  } catch (e) {
+    console.error(e)
+    return NextResponse.json(`Failed to run command ${value} on device ${device}`, {
+      status: 500,
+    })
+  }
+}
