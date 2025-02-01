0bcbbeb9b544fd584b769766d0a3c1147f5fa800
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2025-02-01 18:30:31 -0500

    separate tooltip name and value

--- src/client/components/ui/chart.tsx
+++ src/client/components/ui/chart.tsx
@@ -244,7 +244,7 @@ const ChartTooltipContent = React.forwardRef<
                       </div>
                       {item.value && (
                         <span className="font-mono font-medium tabular-nums text-foreground">
-                          {`${item.value.toLocaleString()}${unit}`}
+                          &nbsp;{`${item.value.toLocaleString()}${unit}`}
                         </span>
                       )}
                     </div>
