ba35ee7dc14d5be3edc18fa23308210bb539497a
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2025-02-13 11:34:43 -0500

    fix sonarqube

--- src/client/components/ui/chart.tsx
+++ src/client/components/ui/chart.tsx
@@ -310,7 +310,7 @@ function getPayloadConfigFromPayload(config: ChartConfig, payload: unknown, key:
     configLabelKey = payloadPayload[key as keyof typeof payloadPayload] as string
   }
 
-  return configLabelKey in config ? config[configLabelKey] : config[key as keyof typeof config]
+  return configLabelKey in config ? config[configLabelKey] : config[key]
 }
 
 export { ChartContainer, ChartTooltip, ChartTooltipContent, ChartLegend, ChartLegendContent, ChartStyle }
