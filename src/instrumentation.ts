cfd6a01cba970aad8b9353fc3742b2faeb0fc0e4
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2025-05-02 17:50:34 -0400

    remove conditional secret

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -4,9 +4,7 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./server/scheduler')
     // Import and run auth secret check
-    if (process.env.WEB_USERNAME && process.env.WEB_PASSWORD) {
-      const { ensureAuthSecret } = await import('./server/auth-config')
-      ensureAuthSecret()
-    }
+    const { ensureAuthSecret } = await import('./server/auth-config')
+    ensureAuthSecret()
   }
 }
