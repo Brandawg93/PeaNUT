17ecdfaa49ce2cb70a3360644bd4ec8fbfd9965a
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2025-08-08 11:12:19 -0400

    remove react compiler

--- next.config.js
+++ next.config.js
@@ -11,10 +11,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    esmExternals: true,
-    reactCompiler: true,
-  },
   turbopack: {
     rules: {
       // Configure any specific rules for your project here
