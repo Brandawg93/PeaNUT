4ae2b199c79fe4f66294fc7df61b90c698cbd06b
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2025-08-15 10:02:43 -0400

    Fix test warnings

--- __tests__/unit/client/components/line-charts/volts-chart.test.tsx
+++ __tests__/unit/client/components/line-charts/volts-chart.test.tsx
@@ -1,29 +1,12 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
 import VoltsChart from '@/client/components/line-charts/volts-chart'
 import { DEVICE } from '@/common/types'
-import { LanguageContext } from '@/client/context/language'
-import { TimeRangeProvider } from '@/client/context/time-range'
-import { SettingsProvider } from '@/client/context/settings'
-import { ThemeProvider } from '@/client/context/theme-provider'
+import { renderWithProviders, waitForSettings } from '../../../../utils/test-utils'
 import { device } from '../../../../__mocks__/chartData'
 
-// Helper function to create a test wrapper component
-const createTestWrapper = (children: React.ReactNode, queryClient: QueryClient) => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-      <SettingsProvider>
-        <TimeRangeProvider>
-          <LanguageContext.Provider value='en'>{children}</LanguageContext.Provider>
-        </TimeRangeProvider>
-      </SettingsProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
-)
-
 // Helper function to render VoltsChart with common props
-const renderVoltsChart = (queryClient: QueryClient, vars: DEVICE['vars'], outputVoltage?: number) => {
+const renderVoltsChart = async (queryClient: QueryClient, vars: DEVICE['vars'], outputVoltage?: number) => {
   const chart = (
     <VoltsChart
       id={device.name}
@@ -34,7 +17,9 @@ const renderVoltsChart = (queryClient: QueryClient, vars: DEVICE['vars'], output
     />
   )
 
-  return render(createTestWrapper(chart, queryClient))
+  const result = renderWithProviders(chart, { queryClient })
+  await waitForSettings()
+  return result
 }
 
 describe('Volts', () => {
@@ -50,15 +35,15 @@ describe('Volts', () => {
     })
   })
 
-  it('renders', () => {
-    const { getByTestId } = renderVoltsChart(queryClient, device.vars)
+  it('renders', async () => {
+    const { getByTestId } = await renderVoltsChart(queryClient, device.vars)
     expect(getByTestId('volts-chart')).toBeInTheDocument()
   })
 
-  it('renders with no output voltage', () => {
+  it('renders with no output voltage', async () => {
     const vars = { ...device.vars }
     delete vars['output.voltage']
-    const { getByTestId } = renderVoltsChart(queryClient, vars)
+    const { getByTestId } = await renderVoltsChart(queryClient, vars)
     expect(getByTestId('volts-chart')).toBeInTheDocument()
   })
 })
