14daf390c723157273238978a62a3e511fcfc481
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2025-08-22 19:38:04 -0400

    normalize base path

--- src/hooks/useBasePath.ts
+++ src/hooks/useBasePath.ts
@@ -3,5 +3,9 @@
 import { env } from 'next-runtime-env'
 
 export function useBasePath(): string {
-  return env('NEXT_PUBLIC_BASE_PATH') || ''
+  const basePath = env('NEXT_PUBLIC_BASE_PATH') || ''
+
+  // Normalize base path to always have a leading slash
+  if (!basePath) return ''
+  return basePath.startsWith('/') ? basePath : `/${basePath}`
 }
