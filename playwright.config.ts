753b30c94663444da61f21ffeb212bf0233685da
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2025-11-10 15:48:42 -0500

    only run docker once for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: 'pnpm run dev',
+    command: 'pnpm run dev:next',
     url: baseURL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
